a4521cdb0c14ab9e8a4db5570b3cb52a6114bb67
Flo_75 <flopereira75@gmail.com>
2022-06-07 23:05:08 +0200

    FEM challenge done

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const quote = document.querySelector('.quote');
+const quoteNb = document.querySelector('.advice-nb');
+const btnNewQuote = document.querySelector('.btn-new-quote');
+
+const getQuote = () => {
+    fetch("https://api.adviceslip.com/advice")
+    .then((res) => res.json())
+    .then((data) => {
+        quote.innerHTML = data.slip.advice;
+        quoteNb.innerHTML = data.slip.id;
+    });
+}
+
+btnNewQuote.addEventListener("click", () => getQuote());
