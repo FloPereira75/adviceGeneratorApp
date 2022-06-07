ff0cd3a339b32f4d8567d55935a119b85bc1359f
Flo_75 <flopereira75@gmail.com>
2022-06-08 01:30:53 +0200

    add quote in innerHTML

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ const getQuote = () => {
     fetch("https://api.adviceslip.com/advice")
     .then((res) => res.json())
     .then((data) => {
-        quote.innerHTML = data.slip.advice;
+        quote.innerHTML = '"' +  data.slip.advice + '"';
         quoteNb.innerHTML = data.slip.id;
     });
 }
